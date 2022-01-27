2af4f3638bc5eaac35fc5df0a36a7273eefd7cbc
ToFJ <jehletom@hotmail.com>
2022-01-27 02:00:50 +0100

    stars now reflecting the rating of the restaurant

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -14,7 +14,7 @@ const RestaurantList = ({ place }) => {
                 src={
                   place.photo
                     ? place.photo.images.large.url
-                    : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                    : "https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
                 }
                 alt={place.name}
               />
@@ -28,9 +28,37 @@ const RestaurantList = ({ place }) => {
                 See on map
               </p>
               <p className="ranking">
-                <span>
-                  <AiFillStar />
-                </span>
+                {place.rating >= 5 ? (
+                  <span>
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                  </span>
+                ) : place.rating >= 4 ? (
+                  <span>
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                  </span>
+                ) : place.rating >= 3 ? (
+                  <span>
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                  </span>
+                ) : place.rating >= 2 ? (
+                  <span>
+                    <AiFillStar />
+                    <AiFillStar />
+                  </span>
+                ) : (
+                  <span>
+                    <AiFillStar />
+                  </span>
+                )}
                 {place.ranking}
               </p>
               <p className="address">
@@ -50,7 +78,7 @@ const RestaurantList = ({ place }) => {
                 ""
               )}
 
-              <a className="website" href={place.web_url}>
+              <a className="website" href={place.web_url} target="_blank" rel="noreferrer">
                 <span>
                   <AiOutlineLink />
                 </span>
@@ -65,3 +93,38 @@ const RestaurantList = ({ place }) => {
 };
 
 export default RestaurantList;
+// {place.rating > 1 ? (
+//   <span>
+//     <AiFillStar />
+//   </span>
+// ) : place.rating > 2 ? (
+//   <span>
+//     <AiFillStar />
+//     <AiFillStar />
+//   </span>
+// ) : place.rating > 3 ? (
+//   <span>
+//     <AiFillStar />
+//     <AiFillStar />
+//     <AiFillStar />
+//   </span>
+// ) : place.rating > 4 ? (
+//   <span>
+//     <AiFillStar />
+//     <AiFillStar />
+//     <AiFillStar />
+//     <AiFillStar />
+//   </span>
+// ) : (
+//   (place.rating = 5 ? (
+//     <span>
+//       <AiFillStar />
+//       <AiFillStar />
+//       <AiFillStar />
+//       <AiFillStar />
+//       <AiFillStar />
+//     </span>
+//   ) : (
+//     ""
+//   ))
+// )}
